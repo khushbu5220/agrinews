59cd717f72edd24f5365f46c6d54c1c5e2320a8e
Khushbu <singhkhushbu210@gmail.com>
2021-04-01 18:17:40 +0530

    news api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import News from "./components/News";
 import './App.css';
 
+
 function App() {
   return (
     <News />
--- src/components/News.js
+++ src/components/News.js
@@ -11,22 +11,31 @@ export default class FetchRandomUser extends React.Component {
   };
 
   async componentDidMount() {
-    const url = "http://newsapi.org/v2/everything?q=Agriculture&from=2021-02-27&sortBy=popularity?country=in&apiKey=7492ea061b604d5b87f1759168417673";
+    const url = "https://newsapi.org/v2/everything?q=Agriculture&from=2021-04-01&sortBy=popularity&apiKey=4dd2d4730f234457af812e76b0192c54";
     const response = await fetch(url);
     const data = await response.json();
     this.setState({ news: data.articles, loading: false });
+ 
   }
 
 
 
   render() {
+    
+    
+    
+
     if (this.state.loading) {
       return <div>loading...</div>;
     }
 
     if (!this.state.news) {
-      return <div>didn't get a news</div>;
+      return (<div>didn't get a news</div>);
     }
+
+      
+
+
 
 
     return (
@@ -34,7 +43,8 @@ export default class FetchRandomUser extends React.Component {
           <img className="logo" src={logo} alt="" />
         <h1 className="heading">AGRINEWS</h1>
 
-          <div className="box">
+
+         <div className="box" id="box">
           <div className="name">#{this.state.news[0].source.name}</div>
           <img className="image" src={this.state.news[0].urlToImage} alt=""/>
           <div className="title">{this.state.news[0].title}</div>
@@ -43,7 +53,18 @@ export default class FetchRandomUser extends React.Component {
         <div className="url"><a href={this.state.news[0].url}>{this.state.news[0].url}</a></div>
         <div className="publish">{this.state.news[0].publishedAt}</div>
        </div>
-       </div>
+
+
+
+
+
+
+    </div>
+
     );
+
+
+
+
   }
 }
